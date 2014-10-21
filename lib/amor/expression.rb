0386aa1c9327371d3715612095b57d71dbc33df6
Florian Dahms <florian.dahms@googlemail.com>
2014-10-21 23:32:26 +0200

    Unary - for Expression

--- lib/amor/expression.rb
+++ lib/amor/expression.rb
@@ -19,10 +19,14 @@ module Amor
 
     def -(value)
       if value.is_a? Expression
-        Expression.new(self.factors + value.factors.map{|factor| [-factor[0], factor[1]]})
+        self + -value
       elsif value.is_a? Variable
         Expression.new(self.factors + [[-1, value]])
       end
+    end
+
+    def -@
+      return Expression.new(self.factors.map{|factor| [-factor[0], factor[1]]})
     end
   end
 end
--- spec/amor/expression_spec.rb
+++ spec/amor/expression_spec.rb
@@ -8,17 +8,17 @@ module Amor
     end
 
     describe '#+' do
-      context 'when used with another expression' do
+      context 'when used with another Expression' do
         before(:each) do
           @other_variable = double('Other Variable')
           @other_expression = Expression.new([[3.0, @other_variable]])
         end
 
-        it 'returns an expression' do
+        it 'returns an Expression' do
           expect(@expression + @other_expression).to be_a(Expression)
         end
 
-        it 'returns an expression with the combined factors of both expressions' do
+        it 'returns an Expression with the combined factors of both Expressions' do
           expect((@expression + @other_expression).factors).to eq([[2, @variable], [3.0, @other_variable]])
         end
       end
@@ -28,28 +28,28 @@ module Amor
           @other_variable = Variable.new(double('Model'),1)
         end
 
-        it 'returns an expression' do
+        it 'returns an Expression' do
           expect(@expression + @other_variable).to be_a(Expression)
         end
 
-        it 'returns an expression with the factor of the variable set to 1' do
+        it 'returns an Expression with the factor of the variable set to 1' do
           expect((@expression + @other_variable).factors).to eq([[2, @variable], [1, @other_variable]])
         end
       end
     end
 
     describe '#-' do
-      context 'when used with another expression' do
+      context 'when used with another Expression' do
         before(:each) do
           @other_variable = double('Other Variable')
           @other_expression = Expression.new([[3.0, @other_variable]])
         end
 
-        it 'returns an expression' do
+        it 'returns an Expression' do
           expect(@expression - @other_expression).to be_a(Expression)
         end
 
-        it 'returns an expression with the combined factors of the first expression and the negative second expression' do
+        it 'returns an Expression with the combined factors of the first Expression and the negative second Expression' do
           expect((@expression - @other_expression).factors).to eq([[2, @variable], [-3.0, @other_variable]])
         end
       end
@@ -59,13 +59,23 @@ module Amor
           @other_variable = Variable.new(double('Model'),1)
         end
 
-        it 'returns an expression' do
+        it 'returns an Expression' do
           expect(@expression - @other_variable).to be_a(Expression)
         end
 
-        it 'returns an expression with the factor of the variable set to -1' do
+        it 'returns an Expression with the factor of the variable set to -1' do
           expect((@expression - @other_variable).factors).to eq([[2, @variable], [-1, @other_variable]])
         end
+      end
+    end
+
+    describe '#-@' do
+      it 'returns an Expression' do
+        expect(-@expression).to be_a(Expression)
+      end
+
+      it 'returns an Expression with the negative factors' do
+        expect((-@expression).factors).to eq([[-2, @variable]])
       end
     end
   end
