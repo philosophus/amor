449474271a1b8adf9f7149136d4ce46a6099adfa
Florian Dahms <florian.dahms@googlemail.com>
2014-10-21 23:09:15 +0200

    Can append variable to expression

--- lib/amor/expression.rb
+++ lib/amor/expression.rb
@@ -1,3 +1,5 @@
+require 'amor/variable'
+
 module Amor
   class Expression
 
@@ -6,9 +8,13 @@ module Amor
     def initialize(factors)
       @factors = factors
     end
-    
+
     def +(value)
-      Expression.new(self.factors + value.factors)
+      if value.is_a? Expression
+        Expression.new(self.factors + value.factors)
+      elsif value.is_a? Variable
+        Expression.new(self.factors + [[1, value]])
+      end
     end
   end
 end
--- spec/amor/expression_spec.rb
+++ spec/amor/expression_spec.rb
@@ -22,6 +22,20 @@ module Amor
           expect((@expression + @other_expression).factors).to eq([[2, @variable], [3.0, @other_variable]])
         end
       end
+
+      context 'when used with a variable' do
+        before(:each) do
+          @other_variable = Variable.new(double('Model'),1)
+        end
+
+        it 'returns an expression' do
+          expect(@expression + @other_variable).to be_a(Expression)
+        end
+
+        it 'returns an expression with the factor of the variable set to 1' do
+          expect((@expression + @other_variable).factors).to eq([[2, @variable], [1, @other_variable]])
+        end
+      end
     end
   end
 end
