bcbf3ebab1a4ddfea6fb49e00ded332ca8c1213d
Florian Dahms <florian.dahms@googlemail.com>
2014-10-21 23:13:08 +0200

    Can subtract an expression

--- lib/amor/expression.rb
+++ lib/amor/expression.rb
@@ -16,5 +16,13 @@ module Amor
         Expression.new(self.factors + [[1, value]])
       end
     end
+
+    def -(value)
+      if value.is_a? Expression
+        Expression.new(self.factors + value.factors.map{|factor| [-factor[0], factor[1]]})
+      elsif value.is_a? Variable
+        Expression.new(self.factors + [[-1, value]])
+      end
+    end
   end
 end
--- spec/amor/expression_spec.rb
+++ spec/amor/expression_spec.rb
@@ -37,5 +37,36 @@ module Amor
         end
       end
     end
+
+    describe '#-' do
+      context 'when used with another expression' do
+        before(:each) do
+          @other_variable = double('Other Variable')
+          @other_expression = Expression.new([[3.0, @other_variable]])
+        end
+
+        it 'returns an expression' do
+          expect(@expression - @other_expression).to be_a(Expression)
+        end
+
+        it 'returns an expression with the combined factors of the first expression and the negative second expression' do
+          expect((@expression - @other_expression).factors).to eq([[2, @variable], [-3.0, @other_variable]])
+        end
+      end
+
+      context 'when used with a variable' do
+        before(:each) do
+          @other_variable = Variable.new(double('Model'),1)
+        end
+
+        it 'returns an expression' do
+          expect(@expression - @other_variable).to be_a(Expression)
+        end
+
+        it 'returns an expression with the factor of the variable set to -1' do
+          expect((@expression - @other_variable).factors).to eq([[2, @variable], [-1, @other_variable]])
+        end
+      end
+    end
   end
 end
