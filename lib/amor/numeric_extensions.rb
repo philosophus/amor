dba1181ef7be849516a902eacdd0b3b995e42894
Florian Dahms <florian.dahms@googlemail.com>
2014-10-21 23:51:47 +0200

    Can multiply a Float with a variable

--- lib/amor/numeric_extensions.rb
+++ lib/amor/numeric_extensions.rb
@@ -9,3 +9,15 @@ class Fixnum
     end
   end
 end
+
+class Float
+  old_multiplication = instance_method(:'*')
+
+  define_method(:'*') do |value|
+    if value.is_a? Amor::Variable
+      Amor::Expression.new([[self, value]])
+    else
+      old_multiplication.bind(self).(value)
+    end
+  end
+end
--- spec/amor/numeric_extensions_spec.rb
+++ spec/amor/numeric_extensions_spec.rb
@@ -1,19 +1,21 @@
 require 'amor/numeric_extensions'
 
 module Amor
-  describe Fixnum do
-    describe '#*' do
-      context 'when used with a variable' do
-        before(:each) do
-          @variable = Variable.new(double('Model'), 1)
-        end
+  [5, 5.0].each do |number|
+    describe "#{number.class}" do
+      describe '#*' do
+        context 'when used with a variable' do
+          before(:each) do
+            @variable = Variable.new(double('Model'), 1)
+          end
 
-        it 'returns an Expression' do
-          expect(5 * @variable).to be_a(Expression)
-        end
+          it 'returns an Expression' do
+            expect(number * @variable).to be_a(Expression)
+          end
 
-        it 'returns an Expression with the factor of the variable set to the number' do
-          expect((5 * @variable).factors).to eq([[5, @variable]])
+          it 'returns an Expression with the factor of the variable set to the number' do
+            expect((number * @variable).factors).to eq([[number, @variable]])
+          end
         end
       end
     end
