2d6be3c27ad9633f38a6956e508d9dda0c922885
Florian Dahms <dahms@or.rwth-aachen.de>
2014-10-22 09:36:45 +0200

    Can subtract constant from variable

--- spec/amor/variable_spec.rb
+++ spec/amor/variable_spec.rb
@@ -56,7 +56,6 @@ module Amor
         end
       end
 
-
       [6, 6.0].each do |number|
         context "when used with a #{number.class}" do
           it 'returns an Expression' do
@@ -97,6 +96,18 @@ module Amor
 
         it 'returns an Expression with the factor of the first variable set to 1 and the second to -1' do
           expect((@variable - @other_variable).factors).to eq([[1, @variable], [-1, @other_variable]])
+        end
+      end
+
+      [6, 6.0].each do |number|
+        context "when used with a #{number.class}" do
+          it 'returns an Expression' do
+            expect(@variable - number).to be_a(Expression)
+          end
+
+          it 'returns an Expression with the first factor set to the variable and the second factor set to the negative constant' do
+            expect((@variable - number).factors).to eq([[1, @variable], [-number, :constant]])
+          end
         end
       end
     end
