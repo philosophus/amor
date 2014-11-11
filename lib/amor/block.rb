e24f5eb6fdac9491d92275e66666b9cccde110de
Florian Dahms <dahms@or.rwth-aachen.de>
2014-11-11 13:53:54 +0100

    Block can returns its .dec representation

--- lib/amor/block.rb
+++ lib/amor/block.rb
@@ -1,6 +1,6 @@
 module Amor
   class Block
-    attr_accessor :constraints
+    attr_reader :constraints
 
     def initialize
       @constraints = Array.new
@@ -8,6 +8,17 @@ module Amor
 
     def add_constraint constraint
       @constraints << constraint
+    end
+
+    # Returns a representation for .dec file format of this block
+    # It assumes the block has the specified index
+    def dec_string index
+      result = ["BLOCK #{index}"]
+      @constraints.each do | constraint |
+        result << constraint.lp_name
+      end
+
+      result.join("\n")
     end
   end
 end
--- spec/amor/block_spec.rb
+++ spec/amor/block_spec.rb
@@ -10,5 +10,18 @@ module Amor
         expect(block.constraints[0]).to eq(constraint)
       end
     end
+
+    describe '#dec_string' do
+      it 'returns a representation of the block for the .dec file format' do
+        constraint1 = double('Constraint')
+        allow(constraint1).to receive(:lp_name).and_return('c1')
+        constraint2 = double('Constraint')
+        allow(constraint2).to receive(:lp_name).and_return('c2')
+        block = Block.new
+        block.add_constraint(constraint1)
+        block.add_constraint(constraint2)
+        expect(block.dec_string(1)).to eq("BLOCK 1\nc1\nc2")
+      end
+    end
   end
 end
